6297d66fe7df2b397299d00634ce34917e005df7
Praveen_Koudagani <Praveen_Koudagani@EPINHYDW05FA>
2022-04-05 04:12:17 +0530

    MaxSumArrayProblem Solution added

---
+++ Spring_WEB_FLUX_Demo/src/main/java/othertasks/MaxSubArray.java
@@ -0,0 +1,48 @@
+package othertasks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MaxSubArray {
+
+	private static final Logger logg = LogManager.getLogger(MaxSubArray.class);
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		logg.info("enter number of elements:");
+		int n = sc.nextInt();
+		List<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			int temp = sc.nextInt();
+			numbers.add(temp);
+		}
+		int ans=0,currentsum=0;
+		int sum=0;
+		int a=0,st=0,ed=0;
+		while(a<n) {
+			sum=sum+numbers.get(a);
+			if((currentsum+numbers.get(a))<=0) {
+				currentsum=0;
+				st=a;
+			}
+			else{
+				currentsum=currentsum+numbers.get(a);
+			}
+			
+			if(currentsum>=ans) {
+				ed=a;
+			}
+			ans=Math.max(ans, currentsum);
+			a++;
+		}
+		logg.info(String.format("Max_Sum:%d",ans));
+		numbers.subList(st+1, ed+1).forEach(i->logg.info(i));
+
+	}
+
+}
